59a759c19ab6c72797a05f3f53792652b59837eb
TomSnowflakeTest <123750317+TomSnowflakeTest@users.noreply.github.com>
2023-01-30 11:58:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # Writes data to the screen
 
 
 # Place fruit details into nice format 
